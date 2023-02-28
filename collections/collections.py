15f64f24c1627a5a91bf2fc70f7d8feb4914fc40
michel <1024836+michel-lacle@users.noreply.github.com>
2023-02-28 13:57:00 -0800

    collections

---
+++ collections/collections.py
@@ -0,0 +1,7 @@
+t = ('aruba', 2, 5.5)
+
+for index, value in enumerate(t):
+    print(f'index: {index}, value: {value}')
+
+for enumaration in enumerate(t):
+    print(f'enumaration: {enumaration}')
